002d3bdf78989654dcec6183b6d61acd17494201
halilfindik <halfin@gmail.com>
2022-08-19 17:06:02 +0300

    First Commit

---
+++ src/test/java/Pages/Parent.java
@@ -0,0 +1,51 @@
+package Pages;
+
+import Utilities.Gwd;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Parent {
+    Duration dr=Duration.ofSeconds(50);
+    public void sendKeysFunction(WebElement element, String value)
+    {
+        waitUntilVisible(element);
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(value);
+    }
+    public void clickFunction(WebElement element)
+    {
+        waitUntilClickable(element);
+        scrollToElement(element);
+        element.click();
+    }
+    public void waitUntilVisible(WebElement element) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebDriverWait wait=new WebDriverWait(Gwd.getDriver(), dr);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js=(JavascriptExecutor) Gwd.getDriver();
+        js.executeScript("arguments[0].scrollIntoView();",element);
+    }
+
+    public void waitUntilClickable(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(Gwd.getDriver(), dr);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void verifyContainsText(WebElement element, String text) {
+        waitUntilVisible(element);
+        Assert.assertTrue(element.getText().toLowerCase().contains(text.toLowerCase()));
+    }
+}
